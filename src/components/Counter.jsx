b2b92c7562756c83366b6ef9bfbe06b55136b032
Kiran <smkiran.09@gmail.com>
2024-07-06 23:38:40 +0530

    Counter with RTK completed

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,24 +1,27 @@
-import { count } from 'console'
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../features/counter/counterSlice";
 
 const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
 
-    const incrementHandler = ()=>{
-console.log("incremented")
-    }
-    const decrementHandler =()=>{
-        console.log("decremented")
+  const dispatch = useDispatch();
 
-    }
+  const incrementHandler = () => {
+    dispatch(increment());
+  };
+  const decrementHandler = () => {
+    if (count > 0) dispatch(decrement());
+  };
   return (
     <div>
-        <div>
-            <button onClick={incrementHandler} >Increment</button>
-        <span>Count: {count}</span>
-        <button onClick={decrementHandler} >Decrement</button>
-        </div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <span style={{ margin: "0 25px " }}>Count: {count}</span>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
